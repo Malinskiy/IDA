134fb364963b9f3e70627c68251834ec4540090f
Malinskiy <anton@malinskiy.com>
2012-08-05 15:58:34 +0400

    Include android icons

--- src/com/samsung/infrastructure/MainActivity.java
+++ src/com/samsung/infrastructure/MainActivity.java
@@ -1,9 +1,13 @@
 package com.samsung.infrastructure;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.widget.Toast;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 
 public class MainActivity extends SherlockActivity {
@@ -17,27 +21,64 @@ public class MainActivity extends SherlockActivity {
         actionBar = getSupportActionBar();
         actionBar.setDisplayShowHomeEnabled(false);
         actionBar.setDisplayShowTitleEnabled(false);
-        //actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        menu.add(R.string.actionbar_item_0)
-                .setIcon(R.drawable.social_share)
-                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        MenuInflater inflater = getSupportMenuInflater();
+        inflater.inflate(R.menu.main,menu);
 
-        menu.add(R.string.actionbar_item_1)
-                .setIcon(R.drawable.av_download)
-                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+//        menu.add(R.string.actionbar_item_0)
+//                .setIcon(R.drawable.social_share)
+//                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+//
+//        menu.add(R.string.actionbar_item_1)
+//                .setIcon(R.drawable.av_download)
+//                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+//
+//        menu.add(R.string.actionbar_item_2)
+//                .setIcon(R.drawable.content_save)
+//                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+//
+//        menu.add(R.string.menu_item_0);
+//        menu.add(R.string.menu_item_1);
+//        menu.add(R.string.menu_item_2);
 
-        menu.add(R.string.actionbar_item_2)
-                .setIcon(R.drawable.content_save)
-                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        return true;
+    }
 
-        menu.add(R.string.menu_item_0);
-        menu.add(R.string.menu_item_1);
-        menu.add(R.string.menu_item_2);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.share:
+                Toast.makeText(this, "Share selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            case R.id.download:
+                Toast.makeText(this, "Download selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            case R.id.save:
+                Toast.makeText(this, "Save selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            case R.id.login:
+                Toast.makeText(this, "Login selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            case R.id.exit:
+                Toast.makeText(this, "Exit selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            default:
+                break;
+        }
 
         return true;
     }
